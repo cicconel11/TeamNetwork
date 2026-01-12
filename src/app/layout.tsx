ad33233773ba90bcb9b881f687bb68352ec69842
cicconel11 <cicconel@umich.edu>
2026-01-11 21:32:56 -0500

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "TeamNetwork",
   description: "Multi-organization hub for members, events, donations, and more",
+  verification: {
+    google: "ABHMicqF2aoLD1T2krG0zYWL9PJXgaQZkMtfxsY46ug",
+  },
 };
 
 export default function RootLayout({
