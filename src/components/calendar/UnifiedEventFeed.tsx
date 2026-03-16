3747bc08404d7f8fe886726e895d0d6c8fd1146e
Matt Leonard <mleonard1616@gmail.com>
2026-03-16 16:42:08 -0700

    perf: extend client calendar range to 180 days for full season coverage

--- src/components/calendar/UnifiedEventFeed.tsx
+++ src/components/calendar/UnifiedEventFeed.tsx
@@ -144,7 +144,7 @@ export function UnifiedEventFeed({ orgId, orgSlug, initialEvents }: UnifiedEvent
     const start = new Date();
     start.setHours(0, 0, 0, 0);
     const end = new Date();
-    end.setDate(end.getDate() + 120);
+    end.setDate(end.getDate() + 180);
     end.setHours(23, 59, 59, 999);
     return { start, end };
   }, []);
